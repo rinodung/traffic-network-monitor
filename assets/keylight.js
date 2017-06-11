f3c73a07067debcafc8b19206d69c009fc979996
nguyenchan <pkcnguyen@gmail.com>
2017-06-11 07:34:18 +0700

    cap nhat style.css

--- assets/keylight.js
+++ assets/keylight.js
@@ -307,7 +307,7 @@ var KeylightWorld = new function() {
 		groupData = JSON.parse(groups);	
 		var traffic ='';
 		
-		$.getJSON( "http://192.168.2.130/index.php/data/get", function(data) {
+		$.getJSON( "http://nguyenchan.ddns.net/traffic-network-monitor/index.php/data/get", function(data) {
 		  	
 		  	keys = [];
 			
